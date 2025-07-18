af0895ccd678a05b636d5e3f648d03eec1d83b7a
ChangG11 <gcmighty@gmail.com>
2025-07-18 09:51:00 -0400

    sus

--- gary_the_snail/bluerov2_movement.py
+++ gary_the_snail/bluerov2_movement.py
@@ -12,7 +12,7 @@ class movement(Node):
         
         self.LIST_MOVES = [("forward", 2), ("counter", 2), ("left", 2), ("clock", 2), ("left", 2), ("counter", 2), ("right", 2), ("clock", 2), ("right", 1), ("left", 1), ("right", 1), ("forward", 1), ("backward", 1), ("forward", 1), ("clock", 2), ("counter", 2), ("down", 1), ("up", 1), ("clock", 2), ("counter", 2), ("down", 1), ("up", 1), ("down", 3), ("up", 1), ("up", 1), ("up", 1), ("counter", 2), ("clock", 2), ("right", 1),("down", 1), ("left", 1), ("right", 1), ("left", 1), ("right", 3), ("left", 2), ("right", 2), ("forward", 3), ("left", 3)] # each move is a tuple(move_type, time)
         #self.LIST_MOVES = [("forward", 2), ("left", 2), ("left", 2), ("right", 2), ("right", 1), ("left", 1), ("right", 1), ("forward", 1), ("backward", 2), ("down", 1), ("up", 1), ("down", 1), ("up", 1), ("down", 3), ("up", 1), ("up", 1), ("up", 1), ("right", 1),("down", 1), ("left", 1), ("right", 1), ("left", 1), ("right", 3), ("left", 2), ("right", 2), ("forward", 3), ("left", 3)] # each move is a tuple(move_type, time)
-
+        self.LIST_MOVES = [("forward", 1),("left", 2)]
         self.pub = self.create_publisher(
             ManualControl,        # the message type
             "/manual_control",    # the topic name
@@ -23,7 +23,7 @@ class movement(Node):
 
         # self.play_moves()
 
-    def publish_move(self, move, pct=0):
+    def publish_move(self, move, t, pct=0):
         """
         Does a move relative to the AUV
 
@@ -68,6 +68,11 @@ class movement(Node):
     def time_move(self, move, t):
         start = time()
 
+        self.move_timer = self.create_timer(
+            0.1,
+            self.publish_move(move, t, pct=30)
+        )
+
         while time() - start < t:
             if time() - start >= 0.8 * t:
                 self.publish_move(move, pct=30 * (time() - start) / t)
@@ -75,7 +80,6 @@ class movement(Node):
                 self.publish_move(move, pct=30.0)
             sleep(0.1)
 
-        self.publish_move("stop")
 
     def play_moves(self):
         self.publish_move("stop")
@@ -84,7 +88,7 @@ class movement(Node):
             self.get_logger().info("started " + move[0])
 
             self.time_move(move[0], move[1])
-
+            self.time_move("stop", 1.0)
             self.get_logger().info("ended " + move[0])
         
         self.publish_move("stop")
