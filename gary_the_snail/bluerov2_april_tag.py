b1df3c0ba99b2a757658fe73397b4d9725bfe400
Eric Zhang <ezhang7708@gmail.com>
2025-07-25 09:43:38 -0400

    e

--- gary_the_snail/bluerov2_april_tag.py
+++ gary_the_snail/bluerov2_april_tag.py
@@ -1,1 +1,34 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+
 from dt_apriltags import Detector
+import matplotlib as plt
+import cv2
+from cv_bridge import CvBridge
+
+class april_tag_detector(Node):
+    def __init__(self):
+        super().__init__("april_tag_detector")
+
+        self.sub_camera = self.create_subscription(
+            Image,
+            "/camera",
+            self.camera_callback,
+            10
+        )
+
+    def camera_callback(self, msg):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        
+        img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+        at_detector = Detector(families='tag36h11', #change later after we know what the families are
+                            nthreads=1,
+                            quad_decimate=1.0,
+                            quad_sigma=0.0,
+                            refine_edges=1,
+                            decode_sharpening=0.25,
+                            debug=0)
+        tags = at_detector.detect(img, estimate_tag_pose=False, camera_params=None, tag_size=None)
